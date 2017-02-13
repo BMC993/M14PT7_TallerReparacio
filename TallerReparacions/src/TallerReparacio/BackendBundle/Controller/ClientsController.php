c6981516076680fb088ba93d63de4bcfbb60f3c7
Unknown <bernabemorales4@gmail.com>
2017-02-13 16:48:37 +0100

    Creació Controller Afegir Client versió Prova, i DB 13-02-2017

---
+++ TallerReparacions/src/TallerReparacio/BackendBundle/Controller/ClientsController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace TallerReparacio\BackendBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use MusicShop\FrontendBundle\Entity\Categories;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class ClientsController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('TallerReparacioBackendBundle:Default:index.html.twig');
+    }
+
+     public function afegirAction(Request $request)
+    {
+        // crea una categoria y le asigna algunos datos ficticios para este ejemplo
+        $Clients= new Clients();
+        $Vehicles = new Vehicles();
+        // $category->setName('tato');
+ 
+        $form = $this->createFormBuilder($Clients)
+            ->add('nIF', TextType::class, array('label' => 'NIF'))
+            ->add('nom', TextType::class, array('label' => 'Nom'))
+            ->add('cognom', TextType::class, array('label' => 'Cognom'))
+            ->add('matricula', TextType::class, array('label' => 'Matricula Cotxe'))
+            ->add('marca', TextType::class, array('label' => 'Marca'))
+            ->add('vehicle', TextType::class, array('label' => 'Vehicle'))
+            ->add('model', TextType::class, array('label' => 'Model'))
+            ->add('tipusCombustible', TextType::class, array('label' => 'Tipus Combustible'))
+            ->add('save', SubmitType::class, array('label' => 'Crear Categoria'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $Clients->setNIF($form->getNIF())
+            $Clients->setNom($form->getNom())
+            $Clients->setCognom($form->getCognom())
+            $Vehicles->setMatricula($form->getMatricula())
+            $Vehicles->setMarca($form->getMarca())
+            $Vehicles->setVehicle($form->getVehicle())
+            $Vehicles->setModel($form->getModel())
+            $Vehicles->setTipusCombustible($form->getTipusCombustible())
+
+
+            // ... perform some action, such as saving the task to the database
+            // for example, if Category is a Doctrine entity, save it!
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+
+            return $this->render('MusicShopBackendBundle:Default:categoryAdded.html.twig', array(
+            'titol' => 'Nova categoria afegida',
+            'name' => $category->getName()));
+        }
+ 
+        return $this->render('MusicShopBackendBundle:Default:addCategory.html.twig', array(
+            'titol' => 'Afegir Categoria',
+            'form' => $form->createView(),
+        ));
+    }
+
+}
+}
---
+++ taller_reparacions.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 13-02-2017 a las 16:46:10
+-- Versión del servidor: 10.1.19-MariaDB
+-- Versión de PHP: 7.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `taller_reparacions`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clients`
+--
+
+CREATE TABLE `clients` (
+  `NIF` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `nom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `cognom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `vehicles_matricula` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vehicles`
+--
+
+CREATE TABLE `vehicles` (
+  `id` int(11) NOT NULL,
+  `matricula` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `marca` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `model` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `tipusCombustible` int(11) NOT NULL,
+  `clients_nif` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_C82E743B416931` (`NIF`),
+  ADD KEY `IDX_C82E7451572B15` (`vehicles_matricula`);
+
+--
+-- Indices de la tabla `vehicles`
+--
+ALTER TABLE `vehicles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_1FCE69FA15DF1885` (`matricula`),
+  ADD KEY `IDX_1FCE69FA35203326` (`clients_nif`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `vehicles`
+--
+ALTER TABLE `vehicles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `clients`
+--
+ALTER TABLE `clients`
+  ADD CONSTRAINT `FK_C82E7451572B15` FOREIGN KEY (`vehicles_matricula`) REFERENCES `vehicles` (`matricula`);
+
+--
+-- Filtros para la tabla `vehicles`
+--
+ALTER TABLE `vehicles`
+  ADD CONSTRAINT `FK_1FCE69FA35203326` FOREIGN KEY (`clients_nif`) REFERENCES `clients` (`NIF`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
