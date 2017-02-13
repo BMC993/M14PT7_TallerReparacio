921268edb4d4fb282a3a607ff0a09353f9490441
Unknown <bernabemorales4@gmail.com>
2017-02-13 15:15:32 +0100

    Creació Entitat Clients

---
+++ TallerReparacions/src/TallerReparacio/BackendBundle/Entity/Clients.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace TallerReparacio\BackendBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Clients
+ *
+ * @ORM\Table(name="clients")
+ * @ORM\Entity(repositoryClass="TallerReparacio\BackendBundle\Repository\ClientsRepository")
+ */
+class Clients
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="NIF", type="string", length=10, unique=true)
+     */
+    private $nIF;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cognom", type="string", length=255)
+     */
+    private $cognom;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nIF
+     *
+     * @param string $nIF
+     *
+     * @return Clients
+     */
+    public function setNIF($nIF)
+    {
+        $this->nIF = $nIF;
+
+        return $this;
+    }
+
+    /**
+     * Get nIF
+     *
+     * @return string
+     */
+    public function getNIF()
+    {
+        return $this->nIF;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Clients
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set cognom
+     *
+     * @param string $cognom
+     *
+     * @return Clients
+     */
+    public function setCognom($cognom)
+    {
+        $this->cognom = $cognom;
+
+        return $this;
+    }
+
+    /**
+     * Get cognom
+     *
+     * @return string
+     */
+    public function getCognom()
+    {
+        return $this->cognom;
+    }
+}
+
